e9aea9767bd924853a94a0a11c20a4b35b2f671a
Nicolas Alejandro Martinez Suria <fyedream@gmail.com>
2020-06-07 19:06:56 -0600

    🚧 Adds structure for price cards

---
+++ src/components/priceCard/PriceCard.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './price-card.scss';
+
+const PriceCard = () => {
+    return (
+        <div className="price-card-container">
+            <h6>Ingresos</h6>
+            <h5>18.677,30</h5>
+            <span>2020 year</span>
+        </div>
+      );
+}
+ 
+export default PriceCard;

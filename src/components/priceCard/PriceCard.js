8eb8864ce9ab7a700b99d3aa417138610be30b20
Nicolas Alejandro Martinez Suria <fyedream@gmail.com>
2020-06-15 21:02:19 -0600

    🚧 Modify PriceCard Component scss

--- src/components/priceCard/PriceCard.js
+++ src/components/priceCard/PriceCard.js
@@ -1,12 +1,17 @@
 import React from 'react';
+import euroIcon from '../../assets/icons/euro.svg'
 import './price-card.scss';
 
 const PriceCard = () => {
     return (
         <div className="price-card-container">
-            <h6>Ingresos</h6>
-            <h5>18.677,30</h5>
-            <span>2020 year</span>
+            <img src={euroIcon} alt=""/>
+
+            <div className="price-info">
+            <span>Ingresos</span>
+            <h2>18.677,30 &euro;</h2>
+            <span>year 2020</span>
+            </div>
         </div>
       );
 }
